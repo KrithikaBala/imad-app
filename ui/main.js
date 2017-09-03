ed99704214d1e38d12da9431aa9b2901244aa413
KrithikaBala <30807731+KrithikaBala@users.noreply.github.com>
2017-09-04 00:37:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 console.log('Loaded!');
 var element = document.getElementById("contentChange");
 element.innerHTML = "I have changed to new value"
+
+var image = document.getElementById("Image");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    image.style.marginLeft = marginLeft + "px";
+}
+image.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+}
