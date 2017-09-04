b1530c96eb2dd1a3b5e3ddf8f65ebd52f50ae440
KrithikaBala <30807731+KrithikaBala@users.noreply.github.com>
2017-09-04 14:57:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,26 @@ function moveRight(){
 image.onclick = function(){
     var interval = setInterval(moveRight, 50);
 }
+
+var button = document.getElementById("counter");
+button.onclick = function(){
+    //create the request
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            //take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   
+   //make the request
+   request.open('GET', 'http://abikirthi.imad.hasura-app.io/counter', true);
+   request.send(null)
+    
+};
