ace93ace39d24736fc173e555aea99eb1e71ea0d
KrithikaBala <30807731+KrithikaBala@users.noreply.github.com>
2017-09-05 23:21:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/counter', function (req, res) {
 
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString(hex);
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input', function (req, res) {
