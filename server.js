d52f1b05b686da3804c2bd004f668d1330f11cc2
KrithikaBala <30807731+KrithikaBala@users.noreply.github.com>
2017-09-05 23:18:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-//var articles = {
+var articles = {
     'article-one': {
         title: "Article-One | Food",
         heading:  "Article-One",
