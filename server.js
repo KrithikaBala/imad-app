8a0519ef57dd059cfbcc0390d4aba5ddf53ac3a2
KrithikaBala <30807731+KrithikaBala@users.noreply.github.com>
2017-09-06 00:25:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/login', function (req, res) {
+app.get('/userlogin', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
 });
 
